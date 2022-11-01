b485519307b4923779f10d66867d58ac703f61f7
painiago <95057970+painiago@users.noreply.github.com>
2022-11-01 10:30:43 -0300

    new att

--- src/script.js
+++ src/script.js
@@ -11,4 +11,22 @@ function typeWriter (elemento){
 const titulo = document.querySelector('#feliz');
 typeWriter(titulo);
 
-// var vid = document.getElementById('#videoLu').play();
+var video = document.querySelector('#videoLu');
+
+var promise = video.play();
+
+// promise won’t be defined in browsers that don't support promisified play()
+if (promise === undefined) {
+  console.log('Promisified video play() not supported');
+} else {
+  promise.then(function() {
+    console.log('Video playback successfully initiated, returning a promise');
+  }).catch(function(error) {
+    console.log('Error initiating video playback: ', error);
+  });
+}
+
+video.onloadedmetadata = function() {
+  var fileName = this.currentSrc.replace(/^.*[\\/]/, '');
+  document.querySelector('#videoSrc').innerHTML = 'Playing video: ' + fileName;
+};
